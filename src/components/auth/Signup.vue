a9dcff595752ca8a40eaf0dbc803ddeb5fc45c74
warwick <warwick.nortier@gmail.com>
2020-03-27 16:43:22 +0200

    modified singup component

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -1,26 +1,33 @@
 <template>
-  <div class="">
-
+  <div class>
     <div class="container" id="SignupContainer">
       <div class="row">
         <div class="col-md-4 offset-md-4">
-          <div class="card text-center card  bg-default mb-3">
-            <div class="card-header">
-              Signup
-            </div>
-            <div class="card-body">
-              <input type="text" id="email" class="form-control input-sm chat-input" placeholder="Email" />
-              <br>
-              <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="Username" />
-              <br>
-              <input type="password" id="userPassword" class="form-control input-sm chat-input" placeholder="Password" />
-            <br>
-              <input type="password" id="repeatUserPassword" class="form-control input-sm chat-input" placeholder="Repeat Password" />
-            </div>
-            <div class="card-footer text-muted">
-              <a href="#" class="btn btn-secondary">Create Account</a>
-              <br>
-            </div>
+          <div class="card text-center card bg-default mb-3">
+            <div class="card-header">Signup</div>
+            <form @submit.prevent="onSubmit">
+              <div class="card-body">
+                <input v-model="email" type="text" id="email" class="form-control input-sm chat-input"
+                  placeholder="Email" />
+                <br />
+                <input v-model="username" type="text" id="userName" class="form-control input-sm chat-input"
+                  placeholder="Username" />
+                <br />
+                <input v-model="password" type="password" id="userPassword" class="form-control input-sm chat-input"
+                  placeholder="Password" />
+                <br />
+                <input v-model="confirmPassword" type="password" id="repeatUserPassword"
+                  class="form-control input-sm chat-input" placeholder="Repeat Password" />
+              </div>
+              <!-- <div class="input inline">
+              <input type="checkbox" class="big-checkbox" id="terms" v-model="terms" />
+              <label for="terms" class="ml-2">Accept Terms of Use</label>
+            </div> -->
+              <div class="card-footer text-muted submit">
+                <button class="btn btn-secondary" type="submit">Create Account</button>
+                <br />
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -32,15 +39,32 @@
 /* eslint-disable */
 
 export default {
-  name: 'Login',
+  data () {
+    return {
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: '',
+    }
+  },
   props: {
     msg: String
+  },
+  methods: {
+    onSubmit(){
+      const formData = {
+        email: this.email,
+        username: this.username,
+        password: this.password,
+        confirmPassword: this.confirmPassword
+      }
+      console.log('formdata', formData)
+    }
   }
 };
 </script>
 
 <style scoped>
-
 #SignupContainer {
   margin-top: 10%;
 }
@@ -49,5 +73,8 @@ export default {
   opacity: 0.93;
 }
 
-
+.big-checkbox {
+  width: 15px;
+  height: 15px;
+}
 </style>
