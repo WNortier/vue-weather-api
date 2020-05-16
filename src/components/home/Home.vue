542cdab401b28357196cd1cbace155e77f682d2f
warwick <warwick.nortier@gmail.com>
2020-05-16 17:41:20 +0200

    broken deploy

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,39 +1,36 @@
 <template>
-<div class="home">
-  <app-signin/>
-  <!-- If you want to add content here, a slot is waiting! -->
-</div>
+  <div class="home">
+    <app-signin />
+    <!-- If you want to add content here, a slot is waiting! -->
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-import Signin from '@/components/auth/Signin.vue'
+import axios from "axios";
+import Signin from "@/components/auth/Signin.vue";
 
 export default {
-  name: 'Home',
-  locationKey: '',
+  name: "Home",
+  locationKey: "",
   components: {
-    'app-signin': Signin
-  },
-   mounted: async function(){
-    //  let vm = this;
-    //  await axios.get('http://dataservice.accuweather.com/locations/v1/cities/search?q=capetown&apikey=Kpz82H3k2hCmDw9TMv78cfqmBkNSdxXS')
-    //    .then(function (response) {
-    //      vm.locationKey = response.data[0].Key
-    //    })
-    //    console.log(this.locationKey)
-    //  await axios.get('http://dataservice.accuweather.com/forecasts/v1/daily/5day/' + this.locationKey).then(function (response) {
-    //    console.log(response.data)
-    //  })
-      //     axios.post("/conn/locations/add-user", formData).then((response) => {
-      //   this.createConfirmation = response.data.message
-      // })
+    "app-signin": Signin
+  }
+  //  mounted: async function(){
+  //  let vm = this;
+  //  await axios.get('http://dataservice.accuweather.com/locations/v1/cities/search?q=capetown&apikey=Kpz82H3k2hCmDw9TMv78cfqmBkNSdxXS')
+  //    .then(function (response) {
+  //      vm.locationKey = response.data[0].Key
+  //    })
+  //    console.log(this.locationKey)
+  //  await axios.get('http://dataservice.accuweather.com/forecasts/v1/daily/5day/' + this.locationKey).then(function (response) {
+  //    console.log(response.data)
+  //  })
+  //     axios.post("/conn/locations/add-user", formData).then((response) => {
+  //   this.createConfirmation = response.data.message
+  // })
 
-     }
-}
+  //  }
+};
 </script>
 
-<style>
-
-
-</style>
+<style></style>
